6a5df91bf1c15b1f02b65c8bf4be14e44f2b995e
ArdaOzturk192 <ardaozturk192@gmail.com>
2024-04-29 21:57:56 +0300

    webpage changes

--- gamedb-frontend/src/App.js
+++ gamedb-frontend/src/App.js
@@ -1,14 +1,38 @@
 import './App.css';
+import React, { useState } from 'react';
+
 
 const  App = () => {
+  const [SignIn, setSignIn] = useState(0);
+  
   return (
     <>
       <Navbar/>
+
       <div className = "SignInButtonDiv">
-      <button className = "SignInButton">
+      <button onClick={() => setSignIn(1)} className = "SignInButton">
         Sign In
       </button>
       </div>
+
+      { SignIn ? 
+      (<>
+      <div class="form__group">
+      <input type="input" class="form__field" placeholder="Name" required=""/>
+      <label for="name" class="form__label">UserName</label>
+      </div>
+
+      <div class="form__group">
+      <input type="input" class="form__field" placeholder="Name" required=""/>
+      <label for="name" class="form__label">Password</label>
+      </div>
+      </>)
+      
+      :(
+      <>
+      </>
+      )
+      }  
     </>
 
   );

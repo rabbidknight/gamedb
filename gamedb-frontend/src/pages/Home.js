3363a75cdc29776a9cb2818ba0bbe8af62e5954c
ArdaOzturk192 <ardaozturk192@gmail.com>
2024-05-08 16:12:29 +0300

    Revert "frontend changes"
    This reverts commit e6bc68dc7667a592f4a692760cce0d159c794858.

--- gamedb-frontend/src/pages/Home.js
+++ gamedb-frontend/src/pages/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
 
     <div className = "SignInButtonDiv">
       <Link to="/register"><button onClick={() => setSignIn(1)} className = "SignInButton">
-        Sign In 2
+        Sign In
       </button></Link>
       </div>
 

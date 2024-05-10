f620840641dd96662b05d6d053ccb0f60e53a9df
ArdaOzturk192 <ardaozturk192@gmail.com>
2024-05-10 13:32:34 +0300

    frontend changes

--- gamedb-frontend/src/pages/Home.js
+++ gamedb-frontend/src/pages/Home.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState, useMemo , memo} from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
 import backgroundImg from "../background.jpg"
+import api from "../api/axiosConfig.js"
+import axios from 'axios';
 
 
 export default function Home() {
+/*
+  const [Games, setGames] = useState();
+
+  const getGames = async()=> {
+    try {
+      const response = await api.get("/463");
+      console.log(response.data);
+      setGames(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getGames();
+  },[])
+*/
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=> {  // when page is loading this is executed
+  loadUsers();
+  },[]);
+
+  const loadUsers = async()=> {
+  const result = await axios.get("http://localhost:8080/users");
+  console.log(result.data)
+  setUsers(result.data);
+  }
+
+
   return (
     <>
     <div  className = "Navbar">
@@ -16,6 +48,7 @@ export default function Home() {
     <div>
       <img className = "Background" src={backgroundImg}/>
     </div>
+    
     
     <div className='Main'>
       

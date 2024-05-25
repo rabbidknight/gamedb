21e039ad26bd0287c8064a291692461563668e8b
Bora Duman <47176909+rabbidknight@users.noreply.github.com>
2024-05-25 21:36:34 +0300

    removeGameFromList added

--- database/gamedb_main_routines.sql
+++ database/gamedb_main_routines.sql
@@ -306,6 +306,46 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `removegamefromlist` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `removegamefromlist`(
+    IN userName VARCHAR(255),
+    IN gameName VARCHAR(255),
+    IN listName VARCHAR(255))
+BEGIN
+    DECLARE curuserID, curgameID INT;
+
+    -- Fetch the UserID for the given username
+    SELECT u.UserID
+    INTO curuserID
+    FROM users u
+    WHERE u.Username = userName;
+    
+    -- Fetch the GameID for the given game name
+    SELECT g.GameID
+    INTO curgameID
+    FROM games g
+    WHERE g.Name = gameName;
+    
+    -- Delete the game from the user's list
+    DELETE FROM userlists
+    WHERE UserID = curuserID
+    AND GameID = curgameID
+    AND ListName = listName;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `searchGame` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -419,4 +459,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-24 23:13:59
+-- Dump completed on 2024-05-25 21:17:13

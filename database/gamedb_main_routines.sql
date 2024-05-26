bf61d390dbe5d6904897a39e2ad8ed81d6b0fc6a
Bora Duman <47176909+rabbidknight@users.noreply.github.com>
2024-05-27 00:13:02 +0300

    added new recommendation algo.

--- database/gamedb_main_routines.sql
+++ database/gamedb_main_routines.sql
@@ -1,6 +1,6 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for macos14 (arm64)
 --
--- Host: localhost    Database: gamedb_main
+-- Host: 127.0.0.1    Database: gamedb_main
 -- ------------------------------------------------------
 -- Server version	8.3.0
 
@@ -218,6 +218,42 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `add_rating` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_rating`(IN inputUsername VARCHAR(255), IN gameName VARCHAR(255), IN inputRating VARCHAR(255))
+BEGIN
+		DECLARE curuserID, curgameID INT;
+		SELECT u.UserID
+        INTO curuserID
+        FROM users u
+        WHERE u.Username = userName;
+        
+        SELECT g.GameID
+        INTO curgameID
+        FROM games g
+        WHERE g.Name = gameName;
+        
+        IF userName IS NOT NULL and gameName IS NOT NULL
+        THEN 
+			IF EXISTS (SELECT * FROM ratings r WHERE r.UserID = curuserID and r.gameID = curgameID)
+			THEN UPDATE ratings re SET ratings = inputRating WHERE re.UserID = curuserID and re.gameID = curgameID;
+            ELSE INSERT INTO ratings (UserID, GameID, rating) VALUES (curuserID, curgameID, inputRating);
+            END IF;
+        END IF;
+    END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `CheckUserLogin` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -235,6 +271,68 @@ BEGIN
         FROM Users 
         WHERE Username = inputUsername AND Password = inputPassword
     ) INTO loginSuccess;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `GetRandomGOTYGames` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetRandomGOTYGames`()
+BEGIN
+    SELECT GameID, Name, Image, ReleaseYear
+    FROM Games
+    WHERE goty = 1
+    ORDER BY RAND()
+    LIMIT 5;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `GetRandomHighRatedGames` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetRandomHighRatedGames`(IN UserID INT)
+BEGIN
+    -- Temporarily store the list of genre IDs associated with the user's lists
+    CREATE TEMPORARY TABLE UserGenres (GenreID INT);
+
+    INSERT INTO UserGenres (GenreID)
+    SELECT DISTINCT gg.GenreID
+    FROM game_genres gg
+    JOIN userlists ul ON ul.GameID = gg.GameID
+    WHERE ul.UserID = UserID;
+
+    -- Select games that have a rating higher than 5 and at least one matching genre with the user's preferred genres
+    SELECT g.GameID, g.Name, g.ConsoleID, g.ReleaseYear, g.DeveloperID, g.List, g.Image, g.Rating
+    FROM games g
+    JOIN game_genres gg ON gg.GameID = g.GameID
+    WHERE g.Rating > 5
+    AND gg.GenreID IN (SELECT GenreID FROM UserGenres)
+    GROUP BY g.GameID, g.Name, g.ConsoleID, g.ReleaseYear, g.DeveloperID, g.List, g.Image, g.Rating
+    ORDER BY RAND()
+    LIMIT 5;
+
+    -- Drop the temporary table
+    DROP TEMPORARY TABLE UserGenres;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -485,4 +583,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-26 17:30:33
+-- Dump completed on 2024-05-27  0:12:34
--- database/gamedb_main_userfavorites.sql
+++ database/gamedb_main_userfavorites.sql
@@ -1,6 +1,6 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for macos14 (arm64)
 --
--- Host: localhost    Database: gamedb_main
+-- Host: 127.0.0.1    Database: gamedb_main
 -- ------------------------------------------------------
 -- Server version	8.3.0
 
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-26 17:30:31
+-- Dump completed on 2024-05-27  0:12:34
